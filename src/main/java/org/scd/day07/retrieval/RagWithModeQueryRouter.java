c6006b229f0279c4cf851f1aa0bb8ab30eb12ae9
sunchengdu <3281328128@qq.com>
2025-06-01 22:55:25 +0800

    model query router

--- src/main/java/org/scd/day07/retrieval/RagWithModeQueryRouter.java
+++ src/main/java/org/scd/day07/retrieval/RagWithModeQueryRouter.java
@@ -1,37 +1,60 @@
-package org.scd.day06.retrieval;
+package org.scd.day07.retrieval;
 
 import dev.langchain4j.community.store.embedding.duckdb.DuckDBEmbeddingStore;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.rag.DefaultRetrievalAugmentor;
 import dev.langchain4j.rag.RetrievalAugmentor;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
-import dev.langchain4j.rag.query.transformer.ExpandingQueryTransformer;
-import dev.langchain4j.rag.query.transformer.QueryTransformer;
+import dev.langchain4j.rag.query.router.LanguageModelQueryRouter;
 import dev.langchain4j.service.AiServices;
 import org.scd.common.Assistant;
-import org.scd.day06.indexing.RagIndexing;
+import org.scd.day05.indexing.RagIndexing;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 import static org.scd.common.Constant.*;
 import static org.scd.common.Utils.startConversationWith;
 
-public class RagWithExpandingQuery {
+public class RagWithModeQueryRouter {
 
-    public static void main(String[] args) {
-        EmbeddingModel embeddingModel = RagIndexing.embeddingModel;
+    private static ContentRetriever dataSourceOne() {
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
         DuckDBEmbeddingStore embeddingStore = DuckDBEmbeddingStore.builder()
                 .filePath(DUCKDB_PATH)
-                .tableName(RagIndexing.RAG_JAVA_TABLE)
+                .tableName(RagIndexing.RAG_QUERY_COMPRESSION_TABLE)
                 .build();
-        ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+        return EmbeddingStoreContentRetriever.builder()
                 .embeddingStore(embeddingStore)
                 .embeddingModel(embeddingModel)
                 .maxResults(2)
                 .minScore(0.6D)
                 .build();
+    }
+
+    private static ContentRetriever dataSourceTwo() {
+        EmbeddingModel embeddingModel = org.scd.day06.indexing.RagIndexing.embeddingModel;
+        DuckDBEmbeddingStore embeddingStore = DuckDBEmbeddingStore.builder()
+                .filePath(DUCKDB_PATH)
+                .tableName(org.scd.day06.indexing.RagIndexing.RAG_JAVA_TABLE)
+                .build();
+        return EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .maxResults(2)
+                .minScore(0.6D)
+                .build();
+    }
+
+    public static void main(String[] args) {
+        Map<ContentRetriever, String> retrieverToDescription = new HashMap<>();
+        retrieverToDescription.put(dataSourceOne(), "人物传记");
+        retrieverToDescription.put(dataSourceTwo(), "Java开发手册");
         ChatModel chatModel = OpenAiChatModel.builder()
                 .baseUrl(OPENAI_BASE_URL)
                 .apiKey(OPENAI_API_KEY)
@@ -39,11 +62,8 @@ public class RagWithExpandingQuery {
                 .logRequests(true)
                 .logResponses(true)
                 .build();
-        // 默认生成3个类似问题输入的查询, 使用同义词或替代句子结构，但它们都应该保留原始含义。
-        QueryTransformer queryTransformer = new ExpandingQueryTransformer(chatModel);
         RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
-                .queryTransformer(queryTransformer)
-                .contentRetriever(contentRetriever)
+                .queryRouter(new LanguageModelQueryRouter(chatModel, retrieverToDescription))
                 .build();
         Assistant assistant = AiServices.builder(Assistant.class)
                 .chatModel(chatModel)
